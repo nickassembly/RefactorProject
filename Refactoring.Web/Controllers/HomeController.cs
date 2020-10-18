cc759381641edbeb9333e4298c3cabbdeaf7a2a2
Nick Guerra <nickg@maliciousgames.com>
2020-10-18 07:25:40 -0500

    extracted district list

--- Refactoring.Web/Controllers/HomeController.cs
+++ Refactoring.Web/Controllers/HomeController.cs
@@ -1,36 +1,43 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using Refactoring.Web.Models;
+using Refactoring.Web.Services.Helpers;
 
-namespace Refactoring.Web.Controllers {
-    public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
+namespace Refactoring.Web.Controllers
+{
+   public class HomeController : Controller
+   {
+      private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
+      public HomeController(ILogger<HomeController> logger)
+      {
+         _logger = logger;
+      }
 
-        public IActionResult Index() {
-            _logger.LogDebug("Index loaded");
-            var districts = new List<string> {
-                "Cambridge", "Downtown", "County", "Middleton"
-            };
-            var viewModel = new OrderFormModel {
-                Districts = districts.Select(d => new SelectListItem {
-                    Value = d.ToLower(), 
-                    Text = d
-                })
-            };
-            return View(viewModel);
-        }
+      public IActionResult Index()
+      {
+         _logger.LogDebug("Index loaded");
+       
+         var viewModel = new OrderFormModel
+         {
+            Districts = District.StandardDistricts.Select(d => new SelectListItem
+            {
+               Value = d.ToLower(),
+               Text = d
+            })
+         };
+         return View(viewModel);
+      }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+      public IActionResult Error()
+      {
+         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+   }
 }
---
+++ Refactoring.Web/Services/Helpers/DistrictHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring.Web.Services.Helpers
+{
+   public static class District
+   {
+      public static string Cambridge => "Cambridge";
+      public static string Downtown => "Downtown";
+      public static string County => "County";
+      public static string Middleton => "Middleton";
+
+      public static IEnumerable<string> StandardDistricts => new List<string>
+      {
+         Cambridge, Downtown, County, Middleton
+      };
+   }
+}
